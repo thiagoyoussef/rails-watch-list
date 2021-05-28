1005e307c65ab30499c187db0d1eb81536df3176
Thiago Youssef <thiagodyoussef@gmail.com>
2021-05-28 16:56:51 -0300

    Added cloudinary

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,6 @@ class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
   has_many :reviews, dependent: :destroy
+  has_one_attached :photo
   validates :name, uniqueness: true, presence: true
 end
